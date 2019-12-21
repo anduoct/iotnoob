9f3808e367d0e2d72fe11ceb8f7a133bcd63539b
910359910@qq.com <910359910@qq.com>
2019-12-21 22:14:08 +0800

    003

--- back-end/app/__init__.py
+++ back-end/app/__init__.py
@@ -1,17 +1,29 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from config import Config
 
+# Flask-SQLalchemy plugin
+db = SQLAlchemy()
+# Flask-Migrate plugin
+migrate = Migrate()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
-    #enable CORS
+    # enable CORS
     CORS(app)
+    # init Flask-SQLAlchemy
+    db.init_app(app)
+    # init Flask-Migrate
+    migrate.init_app(app, db)
 
     #register blueprint 
     from app.api import bp as api_bp
     app.register_blueprint(api_bp, url_prefix = '/api')
 
     return app
+
+from app import models
